4a84642e15c889def25360cc5fe68b7bdfcda760
gilpgijs <gilpgijs@gmail.com>
2021-08-01 14:04:05 -0500

    Corrige colores

--- index.js
+++ index.js
@@ -46,17 +46,25 @@ if (consolaMuestra) {
 }
 
 if (código) {
-  const estilos = getComputedStyle(código);
-  alert(estilos.backgroundColor);
-  if (estilos.backgroundColor === "rgb(0, 0, 0)") {
-    // @ts-ignore
-    editor = CodeMirror(código, {
-      mode: "text/html",
-      theme: "cobalt",
-      extraKeys: { "Ctrl-Space": "autocomplete" },
-      tabSize: 2,
-      lineNumbers: true
-    });
+  if (window.matchMedia) {
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      // @ts-ignore
+      editor = CodeMirror(código, {
+        mode: "text/html",
+        theme: "cobalt",
+        extraKeys: { "Ctrl-Space": "autocomplete" },
+        tabSize: 2,
+        lineNumbers: true
+      });
+    } else {
+      // @ts-ignore
+      editor = CodeMirror(código, {
+        mode: "text/html",
+        extraKeys: { "Ctrl-Space": "autocomplete" },
+        tabSize: 2,
+        lineNumbers: true
+      });
+    }
   } else {
     // @ts-ignore
     editor = CodeMirror(código, {
