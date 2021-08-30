974abdc478dca138c9ad42b7c255b8b68e6f13c3
gilpgijs <gilpgijs@gmail.com>
2021-08-29 20:37:41 -0500

    Mejora el funcionamiento de la consola.

--- adapta.js
+++ adapta.js
@@ -1,28 +1,25 @@
 const cl = console.clear;
 const lo = console.log;
 const err = console.error;
-window.console.clear = (/** @type {any[]} */ ...parámetros) => {
+window.onerror = (evt, src, línea, col, error) => {
+ console.error(`[línea: ${línea}, columna: ${col}] `);
+ console.error(error);
+};
+window.console.clear =
+ /** @param {any[]} parámetros */
+ (...parámetros) => {
   cl.apply(null, parámetros);
-  const consola = window.parent.document.querySelector("#consola");
-  if (consola) {
-    consola.textContent = "";
-  }
-};
-window.console.log = (/** @type {any[]} */ ...parámetros) => {
+  window.top.postMessage({ op: "clear", parámetros }, "*");
+ };
+window.console.log =
+ /** @param {any[]} parámetros */
+ (...parámetros) => {
   lo.apply(null, parámetros);
-  const consola = window.parent.document.querySelector("#consola");
-  if (consola) {
-    const div = document.createElement("div");
-    div.textContent = parámetros.join(" ");
-    consola.append(div);
-  }
-};
-window.console.error = (/** @type {any[]} */ ...parámetros) => {
-  const consola = window.parent.document.querySelector("#consola");
-  if (consola) {
-    err.apply(null, parámetros);
-    const div = document.createElement("div");
-    div.textContent = parámetros.join(" ");
-    consola.append(div);
-  }
-};
+  window.top.postMessage({ op: "log", parámetros }, "*");
+ };
+window.console.error =
+ /** @param {any[]} parámetros */
+ (...parámetros) => {
+  window.top.postMessage({ op: "error", parámetros }, "*");
+  err.apply(null, parámetros);
+ };
--- index.js
+++ index.js
@@ -21,163 +21,191 @@ const ventanaMuestra = document.querySelector("#ventanaMuestra");
 const consolaMuestra = document.querySelector("#consolaMuestra");
 /** @type {HTMLElement|null} */
 const consolaSec = document.querySelector("#consolaSec");
+/** @type {HTMLElement|null} */
+const consola = document.querySelector("#consola");
 
 if (abrir) {
-  abrir.addEventListener("change", archivoAbre);
+ abrir.addEventListener("change", archivoAbre);
 }
 
 if (ejecutar) {
-  ejecutar.addEventListener("click", timeout);
+ ejecutar.addEventListener("click", timeout);
 }
 
 if (códigoMuestra) {
-  códigoActualiza();
-  códigoMuestra.addEventListener("click", códigoActualiza);
+ códigoActualiza();
+ códigoMuestra.addEventListener("click", códigoActualiza);
 }
 
 if (ventanaMuestra) {
-  ventanaSecActualiza();
-  ventanaMuestra.addEventListener("click", ventanaSecActualiza);
+ ventanaSecActualiza();
+ ventanaMuestra.addEventListener("click", ventanaSecActualiza);
 }
 
 if (consolaMuestra) {
-  consolaSecActualiza();
-  consolaMuestra.addEventListener("click", consolaSecActualiza);
+ consolaSecActualiza();
+ consolaMuestra.addEventListener("click", consolaSecActualiza);
 }
 
 if (código) {
-  if (window.matchMedia) {
-    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-      // @ts-ignore
-      editor = CodeMirror(código, {
-        mode: "text/html",
-        theme: "cobalt",
-        extraKeys: { "Ctrl-Space": "autocomplete" },
-        tabSize: 2,
-        lineNumbers: true
-      });
-    } else {
-      // @ts-ignore
-      editor = CodeMirror(código, {
-        mode: "text/html",
-        extraKeys: { "Ctrl-Space": "autocomplete" },
-        tabSize: 2,
-        lineNumbers: true
-      });
-    }
+ if (window.matchMedia) {
+  if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+   // @ts-ignore
+   editor = CodeMirror(código, {
+    mode: "text/html",
+    theme: "cobalt",
+    extraKeys: { "Ctrl-Space": "autocomplete" },
+    tabSize: 2,
+    lineNumbers: true
+   });
   } else {
-    // @ts-ignore
-    editor = CodeMirror(código, {
-      mode: "text/html",
-      extraKeys: { "Ctrl-Space": "autocomplete" },
-      tabSize: 2,
-      lineNumbers: true
-    });
+   // @ts-ignore
+   editor = CodeMirror(código, {
+    mode: "text/html",
+    extraKeys: { "Ctrl-Space": "autocomplete" },
+    tabSize: 2,
+    lineNumbers: true
+   });
   }
-  const texto = decodeURIComponent(location.hash.replace(/^\#/, ""));
-  editor.setValue(texto);
-  guardarActualiza(texto);
-  editor.on("change", contenidoCambia);
+ } else {
+  // @ts-ignore
+  editor = CodeMirror(código, {
+   mode: "text/html",
+   extraKeys: { "Ctrl-Space": "autocomplete" },
+   tabSize: 2,
+   lineNumbers: true
+  });
+ }
+ const texto = decodeURIComponent(location.hash.replace(/^\#/, ""));
+ editor.setValue(texto);
+ guardarActualiza(texto);
+ editor.on("change", contenidoCambia);
 }
 
 function códigoActualiza() {
-  if (códigoMuestra && código) {
-    código.style.display = códigoMuestra.checked ? '' : 'none';
-  }
+ if (códigoMuestra && código) {
+  código.style.display = códigoMuestra.checked ? '' : 'none';
+ }
 }
 
 function ventanaSecActualiza() {
-  if (ventanaMuestra && ventanaSec) {
-    ventanaSec.style.display = ventanaMuestra.checked ? '' : 'none';
-  }
+ if (ventanaMuestra && ventanaSec) {
+  ventanaSec.style.display = ventanaMuestra.checked ? '' : 'none';
+ }
 }
 
 function consolaSecActualiza() {
-  if (consolaMuestra && consolaSec) {
-    consolaSec.style.display = consolaMuestra.checked ? '' : 'none';
-  }
+ if (consolaMuestra && consolaSec) {
+  consolaSec.style.display = consolaMuestra.checked ? '' : 'none';
+ }
 }
 
 function timeout() {
-  setTimeout(ejecuta, 100);
+ setTimeout(ejecuta, 100);
 }
 
 function ejecuta() {
-  if (ventana && código) {
-    const src = consolaMuestra && consolaMuestra.checked ?
-      códigoAdapta() :
-      editor.getValue();
-    ventana.srcdoc = src;
-    setInterval(() => {
-      if (ventanaTítulo && ventana.contentDocument
-        && ventana.contentDocument.title) {
-        ventanaTítulo.textContent = ventana.contentDocument.title;
-      }
-    }, 1000);
-  }
+ if (ventana && código) {
+  const src = consolaMuestra && consolaMuestra.checked ?
+   códigoAdapta() :
+   editor.getValue();
+  ventana.srcdoc = src;
+  setInterval(() => {
+   if (ventanaTítulo && ventana.contentDocument
+    && ventana.contentDocument.title) {
+    ventanaTítulo.textContent = ventana.contentDocument.title;
+   }
+  }, 1000);
+ }
 }
 
 function archivoAbre() {
-  const selección = fileSeleccionado();
-  if (selección) {
-    const reader = new FileReader();
-    reader.onload = () => {
-      if (typeof reader.result === "string" && editor && guardar) {
-        guardar.download = selección.name;
-        editor.setValue(reader.result);
-        guardarActualiza(reader.result);
-      }
-    }
-    reader.onerror = () => {
-      if (reader.error) {
-        errorMuestra(reader.error);
-      }
-    };
-    reader.readAsText(selección);
+ const selección = fileSeleccionado();
+ if (selección) {
+  const reader = new FileReader();
+  reader.onload = () => {
+   if (typeof reader.result === "string" && editor && guardar) {
+    guardar.download = selección.name;
+    editor.setValue(reader.result);
+    guardarActualiza(reader.result);
+   }
   }
+  reader.onerror = () => {
+   if (reader.error) {
+    errorMuestra(reader.error);
+   }
+  };
+  reader.readAsText(selección);
+ }
 }
 
 function fileSeleccionado() {
-  return abrir && abrir.files && abrir.files[0];
+ return abrir && abrir.files && abrir.files[0];
 }
 
 function contenidoCambia() {
-  const texto = editor.getValue();
-  location.hash = encodeURIComponent(texto)
-  guardarActualiza(texto);
+ const texto = editor.getValue();
+ location.hash = encodeURIComponent(texto)
+ guardarActualiza(texto);
 }
 
 /** @param {string} texto */
 function guardarActualiza(texto) {
-  if (guardar) {
-    guardar.href =
-      URL.createObjectURL(new Blob([texto], { type: "text/html" }));
-  }
+ if (guardar) {
+  guardar.href =
+   URL.createObjectURL(new Blob([texto], { type: "text/html" }));
+ }
 }
 
 /** @param {DOMException} e */
 function errorMuestra(e) {
-  console.log(e);
-  alert(e.message);
+ console.log(e);
+ alert(e.message);
 }
 
 async function registraServiceWorker() {
-  try {
-    if (navigator.serviceWorker) {
-      const registro = await navigator.serviceWorker.register("sw.js");
-      console.log("Service Worker registrado.");
-      console.log(registro);
-    }
-  } catch (e) {
-    errorMuestra(e);
+ try {
+  if (navigator.serviceWorker) {
+   const registro = await navigator.serviceWorker.register("sw.js");
+   console.log("Service Worker registrado.");
+   console.log(registro);
   }
+ } catch (e) {
+  errorMuestra(e);
+ }
 }
 
+onmessage = evt => {
+ const { op, parámetros } = evt.data;
+ switch (op) {
+  case "clear":
+   if (consola) {
+    consola.textContent = "";
+   }
+   break;
+  case "log":
+   if (consola) {
+    const div = document.createElement("div");
+    div.textContent = parámetros.join(" ");
+    consola.append(div);
+   }
+   break;
+  case "error":
+   if (consola) {
+    const div = document.createElement("div");
+    div.classList.add("error");
+    div.textContent = parámetros.join(" ");
+    consola.append(div);
+   }
+   break;
+ }
+};
+
 function códigoAdapta() {
-  if (editor) {
-    const src = editor.getValue();
-    return src.replace("<script", /* html */ `<script src="adapta.js"></script><script`);
-  } else {
-    return "";
-  }
+ if (editor) {
+  const src = editor.getValue();
+  return src.replace("<script", /* html */ `<script src="adapta.js"></script><script`);
+ } else {
+  return "";
+ }
 }
