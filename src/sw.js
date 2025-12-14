e6237b349997377679d0a5db311e6034800d2c53
gilpgdm <gilpgdm@gmail.com>
2025-12-13 18:12:31 -0600

    sw 3.1

--- src/sw.js
+++ src/sw.js
@@ -2,7 +2,7 @@ import {manifest, version} from '@parcel/service-worker';
 
 /* Espera 11 minutos después de hacer los cambios en tu sitio, para depués
  * actualizar este archivo. */
-const VERSION = "3.1"
+const VERSION = "3.2"
 
 // Verifica si el código corre dentro de un service worker.
 if (self instanceof ServiceWorkerGlobalScope) {
