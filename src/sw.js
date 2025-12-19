714f9d3522966a24bf13ff706482312726e019d7
gilpgdm <gilpgdm@gmail.com>
2025-12-18 20:09:40 -0600

    sw 3.7

--- src/sw.js
+++ src/sw.js
@@ -1,6 +1,6 @@
 import { manifest, version } from '@parcel/service-worker'
 
-const VERSION = "3.6"
+const VERSION = "3.7"
 
 if (self instanceof ServiceWorkerGlobalScope) {
  self.addEventListener("install",
